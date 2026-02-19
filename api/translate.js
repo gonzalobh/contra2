512567fc441f6a9ea3ac96a720b46836ec0fa23e
gonzalobh <62953503+gonzalobh@users.noreply.github.com>
2026-02-19 09:42:27 +0100

    Update translate.js

--- api/translate.js
+++ api/translate.js
@@ -27,7 +27,7 @@ export default async function handler(req, res) {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
-        Authorization: `Bearer ${process.env.ORTO}`,
+        Authorization: `Bearer ${process.env.CONTRA}`,
       },
       body: JSON.stringify({
         model: "gpt-4o",
